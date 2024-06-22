83e55868e3a284857b34f588157b949caa0adcb5
Debanjan Saha <debanjansaha1992@gmail.com>
2024-06-22 22:53:43 +0530

    Updates

--- src/components/ui/button/button.tsx
+++
@@ -1,8 +0,0 @@
-import { ButtonProps } from "./types";
-import styles from "./button.module.scss";
-
-const Button = ({ children }: ButtonProps) => {
-    return <button className={styles.btn}>{children}</button>;
-};
-
-export default Button;
--- src/components/ui/pill/types.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export interface PillProps {
-    label: string;
-    backgroundColor: string;
-    color?: string;
-    icon?: ReactNode;
-    style?: React.CSSProperties;
-}
